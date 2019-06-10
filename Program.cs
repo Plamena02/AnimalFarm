5401531b72022c05b80a28da7e68a4ae1b08b6bf
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-06-10 12:26:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
+namespace AnimalFarm.Model
+{
+    using System;
+    using Chicken;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Chicken chicken = new Chicken(name, age);
+            Console.WriteLine(
+                "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                chicken.Name,
+                chicken.Age,
+                chicken.ProductPerDay);
+        }
+    }
+}
 
